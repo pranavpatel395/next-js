c62c97ca8d7b4108d9f683992c2ed1e7565977cd
pranavpatel395 <pranavpatel828@gmail.com>
2024-12-15 14:53:49 +0530

    course Section Done

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
+import Courses from "@/components/CorsesSections"
 
 export default function Home() {
   return (
+    <>
    <div className="min-h-screen bg-black/[0.96] bg-grid-white/[0.02] antialiased">
     <HeroSection/>
    </div>
+   <div>
+      <Courses/>
+   </div>
+   </>
   );
 }
---
+++ my-app/src/components/CorsesSections.tsx
@@ -0,0 +1,51 @@
+"use client"
+import Link from "next/link"
+import Datacourses from "@/data/corses.json"
+import { BackgroundGradient } from "@/components/ui/background-gradient"
+
+interface Corses{
+  id: number,
+  title: string,
+  slug: string,
+  description: string,
+  price: number,
+  
+  isFeatured: boolean
+
+}
+
+const CorsesSections = () => {
+
+  const featureCorses= Datacourses.corses.filter((corses:Corses) => corses.isFeatured )
+
+  return (
+    <div className="h-auto bg-gray-900">
+      <div className="text-center  "><h1 className=" text-green-300 pt-8 text-3xl">
+      Feature Courses</h1>
+      <p className="text-4xl">Learn Courses Now </p>
+      </div>
+      <div className=" mt-10 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 justify-center">
+            {featureCorses.map((corses:Corses)=>(
+            <div key={corses.id} className="flex justify-center">
+              <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{corses.title}</p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400">{corses.description}</p>
+                  <Link className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800" href={`/corses/${corses.slug}`}>Learn More</Link>
+                </div>
+              </BackgroundGradient>
+            </div>
+            ))}
+        </div>
+      </div>
+      <div className="text-center mt-12 text-xl " >
+        <Link className="bg-black rounded w-14 h-36" href={"/courses"} >
+        see All
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default CorsesSections
